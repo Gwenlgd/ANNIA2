83c0078f81ba70b9bcd65896f7f2b63ab47822d9
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2023-02-15 17:29:18 +0100

    Init: Index's links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+  get '/trajet', to: 'pages#trajet'
+  get '/lieu', to: 'pages#lieu'
+  get '/programme', to: 'pages#programme'
+  get '/affaires', to: 'pages#affaires'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
